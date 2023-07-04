ad940e39a5ef2195a7f5195ecec0c8d542c715c1
AntadShraddha <118249204+AntadShraddha@users.noreply.github.com>
2023-07-04 17:41:08 +0530

    Add files via upload

---
+++ mysql.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE emp(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL ,email NOT NULL );
+
+-- INSERT INTO emp(empid,empname,email)
+-- VAlUES (1,"Sam","shr@ddha.com")
+
+SELECT * FROM emp;
